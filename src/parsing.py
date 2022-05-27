69d0ab2afbc37387f85a8929601943e08a149ab8
Daniel Gesua <DanielGesua@gmail.com>
2022-05-26 23:50:18 -0700

    Add NOTICE file and Apache boilerplate.

--- src/parsing.py
+++ src/parsing.py
@@ -1,10 +1,30 @@
+#
+#    HyperKyube: OCR Gui MultiTool.
+#
+#    Copyright 2022 Daniel Gesua
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#   
+
 from __future__ import annotations
+
 
 import re
 from types import SimpleNamespace
 from typing import List
 from pydantic.dataclasses import dataclass
 from dataclasses import astuple
+
 
 SPLITTING_PATTERN = r'''(?P<text>.)\s # String containing the letter on that row
     (?P<left>\d+)\s      # Left edge displacement value
@@ -13,6 +33,7 @@ SPLITTING_PATTERN = r'''(?P<text>.)\s # String containing the letter on that row
     (?P<top>\d+)\s\d+\n  # Top edge displacement value'''
 
 letter_splitter = re.compile(SPLITTING_PATTERN,flags=re.VERBOSE)
+
 
 @dataclass
 class Displacements():
--- src/tooltips.py
+++ src/tooltips.py
@@ -1,8 +1,29 @@
+#
+#    HyperKyube: OCR Gui MultiTool.
+#
+#    Copyright 2022 Daniel Gesua
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#   
+
+
 from __future__ import annotations
+
 
 from idlelib.tooltip import Hovertip
 from global_scope import real_global_scope as the
 from geometry import WordBox, NoActiveWordBox
+
 
 class WordBoxToolTip(Hovertip):
     ''' A tooltip that appears at the bottom '''
